edbaea741f3ae7d78bcc3d69f4b586a3c9be3deb
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-28 07:57:32 -0700

    responsiveness for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@ var now = moment().format("MMMM Do, YYYY");
 
 var displayTime = document.getElementById("currentDay");
 displayTime.innerHTML = now;
+
+// var saveTask = function() {
+//     localStorage.setItem()
+// }
