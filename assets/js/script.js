4595b2de66803cf9598d7c1b19ca855e69959106
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-30 13:02:50 -0700

    color coded past, present and future time inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,33 @@
 var now = moment().format("MMMM Do, YYYY");
 
+//displays the current date in the Header
 var displayTime = document.getElementById("currentDay");
 displayTime.innerHTML = now;
 
 // var saveTask = function() {
 //     localStorage.setItem()
 // }
+
+
+var hour = moment().hour();
+
+//function to show colors of past, present and future times for user alert
+var colorTime = function() {
+    $(".input").each(function () {
+        var inputId = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+
+        if (hour > inputId) {
+            $(this).addClass("past");
+        } else if (hour < inputId) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present")
+        }
+        console.log(inputId, "input");
+        console.log(hour, "hour");
+
+    });
+}
+
+colorTime();
