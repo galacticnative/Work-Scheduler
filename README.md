c2803c5860e575228545c5e6675c72f095db19f0
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-30 15:47:47 -0700

    set to local storage

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 A business day scheduler for a 9-5 job that you can keep track of hourly tasks. The current time will alert a background color of red. Hours that have passed will be greyed out. Future hours will be plain white.
 
 ## Languages
-HTML, CSS
+HTML, CSS, JavaScript with the implementation of third-party APIs (Bootstrap, MomentJs,JQuery, Iconic)
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,33 @@ var now = moment().format("MMMM Do, YYYY");
 var displayTime = document.getElementById("currentDay");
 displayTime.innerHTML = now;
 
-// var saveTask = function() {
-//     localStorage.setItem()
-// }
+
+
+//save button clicked function
+$(document).ready(function () {
+    $(".saveBtn").click(function() {
+
+        var inputText = $(this).siblings("input").val().trim();
+        var hour = $(this).siblings("section").text()
+
+        localStorage.setItem(hour, inputText);
+    
+        //console.log("save clicked")
+    
+    })
+    
+});
+
+$("#9am").val(localStorage.getItem("9AM"));
+$("#10am").val(localStorage.getItem("10AM"));
+$("#11am").val(localStorage.getItem("11AM"));
+$("#12pm").val(localStorage.getItem("12PM"));
+$("#13pm").val(localStorage.getItem("1PM"));
+$("#14pm").val(localStorage.getItem("2PM"));
+$("#15pm").val(localStorage.getItem("3PM"));
+$("#16pm").val(localStorage.getItem("4PM"));
+$("#17pm").val(localStorage.getItem("5PM"));
+
 
 
 var hour = moment().hour();
@@ -24,10 +48,11 @@ var colorTime = function() {
         } else {
             $(this).addClass("present")
         }
-        console.log(inputId, "input");
-        console.log(hour, "hour");
+        //console.log(inputId, "input");
+        //console.log(hour, "hour");
 
     });
 }
 
+//calls function to show colors
 colorTime();
